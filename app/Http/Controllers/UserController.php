bae08496357767a4eeafa0c546421752f61fe44e
joey <prclizongjun@gmail.com>
2019-08-28 23:51:19 +0800

    第一次提交

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller{
+    public function test(){
+        $data = DB::table('admin_operation_log')->select('*')->get();
+        //dd($data);
+        $results = Article::all();
+        dd($results);
+    }
+}
