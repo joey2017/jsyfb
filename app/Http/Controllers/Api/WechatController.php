314f7f6446155a10471be46a6a98698313f6e2f3
joey <gxlclizongjun@163.com>
2019-10-17 09:25:44 +0800

    授权修改

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -145,7 +145,7 @@ class WechatController extends Controller
     protected function createUser($request, $row, $data, $inviter_id = null)
     {
         return User::create([
-            'username'        => $row['openid'],
+            'username'        => $row['openid'].substr(uniqid(), 7),
             'password'        => bcrypt($row['openid']),
             //'mobile'          => $data['mobile'],
             'openid'          => $row['openid'],
