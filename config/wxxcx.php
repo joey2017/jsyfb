220b48f9b64d4f108546d27730abfc797b53cb40
joey <gxlclizongjun@163.com>
2019-10-22 08:49:12 +0800

    修改appid

--- config/wxxcx.php
+++ config/wxxcx.php
@@ -5,12 +5,12 @@ return [
 	 * 小程序APPID
 	 */
     //'appid' => 'wx153e966429b4ca44',//测试
-    'appid' => 'wxd3ec6734e111d1c9',//测试
+    'appid' => 'wxd3ec6734e111d1c9',
     /**
      * 小程序Secret
      */
     //'secret' => 'dc11be74f7690bf6480de089ee91cdde',//测试
-    'secret' => 'be7da3a042cd608086d42f17615b4aec',//测试
+    'secret' => 'be7da3a042cd608086d42f17615b4aec',
     /**
      * 小程序登录凭证 code 获取 session_key 和 openid 地址，不需要改动
      */
