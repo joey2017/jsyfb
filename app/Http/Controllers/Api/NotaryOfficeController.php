30e47db0befef2d7152830e64f37e17c9d1271cf
joey <prclizongjun@gmail.com>
2019-10-24 06:14:09 +0800

    距离计算

--- app/Http/Controllers/Api/NotaryOfficeController.php
+++ app/Http/Controllers/Api/NotaryOfficeController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\Api\NotaryOfficeResource;
 use App\Models\NotaryOffice;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class NotaryOfficeController extends Controller
 {
@@ -28,12 +30,14 @@ class NotaryOfficeController extends Controller
      */
     public function index(Request $request)
     {
-        $from    = $request->input('location');
-        $notarys = NotaryOffice::paginate(10);
-        foreach ($notarys as &$notary) {
+        $from = $request->input('location');
+        //$notarys = NotaryOffice::paginate(10);
+        $notarys = DB::table((new NotaryOffice)->getTable())->paginate(10)->toArray();
+        foreach ($notarys['data'] as &$notary) {
             $notary->distance = $this->distance($from, [$notary->lat, $notary->lng]);
         }
-        return $this->success(NotaryOfficeResource::collection($notarys));
+        //return $this->success(NotaryOfficeResource::collection($notarys));
+        return $this->success($notarys);
     }
 
 
@@ -74,10 +78,18 @@ class NotaryOfficeController extends Controller
                 'key'  => env('TENCENT_MAP_API_KEY'),
             ];
             //$url    = $url . http_build_query($params);
-
             $client   = new Client();
-            $response = $client->request('GET', $url, ['query' => $params]);
-            $result   = \GuzzleHttp\json_decode($response->getBody(), true);
+
+            try {
+                $response = $client->request('GET', $url, ['query' => $params]);
+                $result   = \GuzzleHttp\json_decode($response->getBody(), true);
+                if ($result['status'] == 0) {
+                    $result = $result['result']['elements'][0]['distance'];
+                }
+            } catch (\Exception $exception) {
+                Log::error('调用腾讯地图接口计算距离失败：' . $exception->getMessage(), ['info' => $exception->getTraceAsString()]);
+            }
+
         }
         return $result;
     }
