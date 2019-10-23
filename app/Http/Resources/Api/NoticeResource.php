bfcd04cf93ea69821d9fa18216591ad160b3b62e
joey <gxlclizongjun@163.com>
2019-10-23 16:47:38 +0800

    用户消息增加状态字段

--- app/Http/Resources/Api/NoticeResource.php
+++ app/Http/Resources/Api/NoticeResource.php
@@ -16,14 +16,15 @@ class NoticeResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'         => $this->id,
-            'user_id'    => $this->user_id,
-            'type'       => Notice::TYPES[$this->type],
-            'title'      => $this->title,
-            'content'    => $this->content,
-            'status'     => Notice::getStatusName($this->status),
-            'created_at' => (string)$this->created_at,
-            'updated_at' => (string)$this->updated_at
+            'id'          => $this->id,
+            'user_id'     => $this->user_id,
+            'type'        => Notice::TYPES[$this->type],
+            'title'       => $this->title,
+            'content'     => $this->content,
+            'status'      => $this->status,
+            'status_text' => Notice::getStatusName($this->status),
+            'created_at'  => (string)$this->created_at,
+            'updated_at'  => (string)$this->updated_at
         ];
     }
 }
