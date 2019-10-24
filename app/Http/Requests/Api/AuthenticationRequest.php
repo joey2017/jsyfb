c8781e0266afcebe568bd25a4fe6f646b7593332
joey <gxlclizongjun@163.com>
2019-10-24 16:58:34 +0800

    实名认证信息修改

--- app/Http/Requests/Api/AuthenticationRequest.php
+++ app/Http/Requests/Api/AuthenticationRequest.php
@@ -17,13 +17,7 @@ class AuthenticationRequest extends FormRequest
             'realname'      => 'required|string',
             'mobile'        => 'required|regex:/^1[345789][0-9]{9}$/',
             //'email'         => 'sometimes|required|email',
-            //'province'      => 'required|string',
-            //'city'          => 'required|string',
-            //'district'      => 'required|string',
-            //'address'       => 'required|string',
             'identity_card' => 'required|string',
-            //'front_photo'   => 'required|string',
-            //'back_photo'    => 'required|string',
             'hand_photo'    => 'required|string',
         ];
     }
@@ -34,14 +28,7 @@ class AuthenticationRequest extends FormRequest
             'realname.required'      => '真实姓名必须填写',
             'mobile.required'        => '手机号码必须填写',
             'mobile.regex'           => '手机号码格式不正确',
-            'email.required'         => '邮箱地址不正确',
-            'province.required'      => '省份必须填写',
-            'city.required'          => '城市必须填写',
-            'district.required'      => '区县必须填写',
-            'address.required'       => '详细地址必须填写',
             'identity_card.required' => '身份证号码必须填写',
-            'front_photo.required'   => '身份证正面照必须填写',
-            'back_photo.required'    => '身份证反面照必须填写',
             'hand_photo.required'    => '手持身份证照必须填写',
         ];
     }
