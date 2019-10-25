eff7b7dea6b43e69da06fb5f573038bc0b86e221
joey <gxlclizongjun@163.com>
2019-10-25 14:59:00 +0800

    用户认证状态

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Api;
 
 use App\Constant;
+use App\Models\Authentication;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,29 +18,30 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'              => $this->id,
-            'username'        => $this->username,
-            'nickname'        => $this->nickname,
-            'avatar'          => $this->avatar,
-            'email'           => $this->email,
-            'openid'          => $this->openid,
-            'gender'          => Constant::SEXS[$this->gender ?? 1],
-            'mobile'          => $this->mobile,
-            'inviter_id'      => $this->inviter_id,
-            'descr'           => $this->descr,
-            'login_num'       => $this->login_num,
-            'reg_ip'          => $this->reg_ip,
-            'last_login_ip'   => $this->last_login_ip,
-            'last_login_time' => (string)$this->last_login_time,
-            'is_verified'     => $this->is_verified,
-            'verified_status' => $this->authen()->first()->status,
-            'ingots'          => $this->ingots,
-            'invitation_code' => $this->invitation_code,
-            'is_vip'          => $this->is_vip,
-            'score'           => $this->score,
-            'status'          => User::getStatusName($this->status ?? 1),
-            'created_at'      => (string)$this->created_at,
-            'updated_at'      => (string)$this->updated_at
+            'id'                   => $this->id,
+            'username'             => $this->username,
+            'nickname'             => $this->nickname,
+            'avatar'               => $this->avatar,
+            'email'                => $this->email,
+            'openid'               => $this->openid,
+            'gender'               => Constant::SEXS[$this->gender ?? 1],
+            'mobile'               => $this->mobile,
+            'inviter_id'           => $this->inviter_id,
+            'descr'                => $this->descr,
+            'login_num'            => $this->login_num,
+            'reg_ip'               => $this->reg_ip,
+            'last_login_ip'        => $this->last_login_ip,
+            'last_login_time'      => (string)$this->last_login_time,
+            'is_verified'          => $this->is_verified,
+            'verified_status'      => $this->authen()->first()->status,
+            'verified_status_text' => Authentication::getStatusName($this->authen()->first()->status),
+            'ingots'               => $this->ingots,
+            'invitation_code'      => $this->invitation_code,
+            'is_vip'               => $this->is_vip,
+            'score'                => $this->score,
+            'status'               => User::getStatusName($this->status ?? 1),
+            'created_at'           => (string)$this->created_at,
+            'updated_at'           => (string)$this->updated_at
         ];
     }
 }
