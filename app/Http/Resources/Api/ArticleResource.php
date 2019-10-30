a4ac5bb35f1383d7b2e8f1dcf51241fa86207ff7
joey <gxlclizongjun@163.com>
2019-10-30 17:55:57 +0800

    文章页返回点赞状态

--- app/Http/Resources/Api/ArticleResource.php
+++ app/Http/Resources/Api/ArticleResource.php
@@ -3,7 +3,9 @@
 namespace App\Http\Resources\Api;
 
 use App\Models\Article;
+use App\Models\ArticleLike;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleResource extends JsonResource
 {
@@ -25,6 +27,7 @@ class ArticleResource extends JsonResource
             'browse_count' => $this->browse_count,
             'like_count'   => $this->like_count,
             'share_count'  => $this->share_count,
+            'is_like'      => ArticleLike::where([['user_id', Auth::guard('api')->id()], ['article_id', $this->id]])->first() ? true : false,
             'status'       => Article::getStatusName($this->status),
             'created_at'   => (string)$this->created_at,
             'updated_at'   => (string)$this->updated_at
