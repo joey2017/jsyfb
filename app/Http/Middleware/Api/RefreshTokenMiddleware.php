bfdee9e2c0515dedd6d644f97b6af7a3d89cfe56
joey <gxlclizongjun@163.com>
2019-11-01 16:40:50 +0800

    tokenRefresh

--- app/Http/Middleware/Api/RefreshTokenMiddleware.php
+++ app/Http/Middleware/Api/RefreshTokenMiddleware.php
@@ -4,10 +4,10 @@ namespace App\Http\Middleware\Api;
 
 use Auth;
 use Closure;
+use Illuminate\Support\Facades\Log;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
-use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 // 我们要继承的是jwt的BaseMiddleware
@@ -25,20 +25,9 @@ class RefreshTokenMiddleware extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
-        //return $next($request);
 
         // 检查此次请求中是否带有 token，如果没有则抛出异常。
         $this->checkForToken($request);
-
-        //获取当前token
-        $token = Auth::guard('api')->getToken();
-
-        //$payload = Auth::guard('api')->manager()->getJWTProvider()->decode($token->get());
-
-//        //比较过期时间
-//        if(empty($payload['exp']) || $payload['exp'] < time()){
-//            throw new TokenInvalidException();
-//        }
 
         // 使用 try 包裹，以捕捉 token 过期所抛出的 TokenExpiredException  异常
         try {
@@ -48,6 +37,7 @@ class RefreshTokenMiddleware extends BaseMiddleware
             }
             throw new UnauthorizedHttpException('jwt-auth', '未登录');
         } catch (TokenExpiredException $exception) {
+            Log::notice('TokenExpiredException:' . $exception->getMessage(), ['info' => $exception->getTraceAsString()]);
             // 此处捕获到了 token 过期所抛出的 TokenExpiredException 异常，我们在这里需要做的是刷新该用户的 token 并将它添加到响应头中
             try {
                 // 刷新用户的 token
@@ -55,10 +45,11 @@ class RefreshTokenMiddleware extends BaseMiddleware
                 // 使用一次性登录以保证此次请求的成功
                 Auth::guard('api')->onceUsingId($this->auth->manager()->getPayloadFactory()->buildClaimsCollection()->toPlainArray()['sub']);
 
-                $user = Auth::guard('api')->user();
+                $user             = Auth::guard('api')->user();
                 $user->last_token = $token;
                 $user->save();
             } catch (JWTException $exception) {
+                Log::warning('JWTException:' . $exception->getMessage(), ['info' => $exception->getTraceAsString()]);
                 // 如果捕获到此异常，即代表 refresh 也过期了，用户无法刷新令牌，需要重新登录。
                 throw new UnauthorizedHttpException('jwt-auth', $exception->getMessage());
             }
