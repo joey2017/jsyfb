876057e9e3996b9fb6e840f751687d4e15203192
joey <prclizongjun@gmail.com>
2019-11-04 00:33:22 +0800

    公证处列表距离返回修改

--- app/Http/Controllers/Api/NotaryOfficeController.php
+++ app/Http/Controllers/Api/NotaryOfficeController.php
@@ -25,7 +25,7 @@ class NotaryOfficeController extends Controller
      */
     public function index(Request $request)
     {
-        $from = $request->input('location');
+        $from    = $request->input('location');
         $notarys = DB::table((new NotaryOffice)->getTable())->paginate(10)->toArray();
         foreach ($notarys['data'] as &$notary) {
             $notary->picture  = env('APP_UPLOAD_PATH') . '/' . $notary->picture;
@@ -93,6 +93,80 @@ class NotaryOfficeController extends Controller
 
         }
         return $result;
+    }
+
+    /**
+     * @param $location
+     * @return \Illuminate\Support\Collection
+     */
+    protected function query($location)
+    {
+        return NotaryOffice::whereBetween('lat', [$location['left-bottom']['lat'], $location['left-top']['lat']])
+            ->whereBetween('lng', [$location['left-bottom']['lng'], $location['right-bottom']['lng']])
+            ->orderBy('lat', 'asc')->orderBy('lng', 'asc')->get();
+    }
+
+    /**
+     * 计算某个经纬度的周围某段距离的正方形的四个点
+     * @param $lng  float 经度
+     * @param $lat  float 纬度
+     * @param int $distance 该点所在圆的半径，该圆与此正方形内切，默认值为1千米
+     * @param int $radius 地球半径 平均6371km
+     * @return array         正方形的四个点的经纬度坐标
+     */
+    public function returnSquarePoint($lng, $lat, $distance = 1, $radius = 6371)
+    {
+        $dlng = 2 * asin(sin($distance / (2 * $radius)) / cos(deg2rad($lat)));
+        $dlng = rad2deg($dlng);
+
+        $dlat = $distance / $radius;
+        $dlat = rad2deg($dlat);
+
+        return array(
+            'left-top'     => array(
+                'lat' => $lat + $dlat,
+                'lng' => $lng - $dlng
+            ),
+            'right-top'    => array(
+                'lat' => $lat + $dlat,
+                'lng' => $lng + $dlng
+            ),
+            'left-bottom'  => array(
+                'lat' => $lat - $dlat,
+                'lng' => $lng - $dlng
+            ),
+            'right-bottom' => array(
+                'lat' => $lat - $dlat,
+                'lng' => $lng + $dlng
+            )
+        );
+    }
+
+    /**
+     * @param $lat1
+     * @param $lng1
+     * @param $lat2
+     * @param $lng2
+     * @return float
+     */
+    public function getDistance($lat1, $lng1, $lat2, $lng2)
+    {
+        $earthRadius = 6367000; //定义地球半径
+
+        $lat1 = ($lat1 * pi()) / 180;
+        $lng1 = ($lng1 * pi()) / 180;
+
+        $lat2 = ($lat2 * pi()) / 180;
+        $lng2 = ($lng2 * pi()) / 180;
+
+        $calcLongitude      = $lng2 - $lng1;
+        $calcLatitude       = $lat2 - $lat1;
+        $stepOne            = pow(sin($calcLatitude / 2), 2) + cos($lat1)
+            * cos($lat2) * pow(sin($calcLongitude / 2), 2);
+        $stepTwo            = 2 * asin(min(1, sqrt($stepOne)));
+        $calculatedDistance = $earthRadius * $stepTwo;
+
+        return round($calculatedDistance);
     }
 
 
