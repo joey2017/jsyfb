4e68c69341405fcb74226a52e6cbf640268088ea
zongjun <zongjun.li@outlook.com>
2019-11-14 14:10:53 +0800

    菜单取消绑定权限

--- config/admin.php
+++ config/admin.php
@@ -329,7 +329,7 @@ return [
     |
     | whether enable menu bind to a permission
     */
-    'menu_bind_permission'      => true,
+    'menu_bind_permission'      => false,
 
     /*
     |--------------------------------------------------------------------------
